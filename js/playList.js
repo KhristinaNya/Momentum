cb0a6fff299a44c6f07225864e8c7c9cc0d0e311
Kris S <kristina19960712@gmail.com>
2022-08-17 00:20:28 +0300

    feat: add new points

--- js/playList.js
+++ js/playList.js
@@ -2,20 +2,12 @@ const playList = [
     {
         title: 'Aqua Caelestis',
         src: '../assets/sounds/Aqua Caelestis.mp3',
-<<<<<<< HEAD
-        duration: '00:58'
-=======
         duration: '00:39'
->>>>>>> main
     },
     {
         title: 'River Flows In You',
         src: '../assets/sounds/River Flows In You.mp3',
-<<<<<<< HEAD
-        duration: '03:50'
-=======
         duration: '01:37'
->>>>>>> main
     },
     {
         title: 'Ennio Morricone',
@@ -24,11 +16,7 @@ const playList = [
     },
     {
         title: 'Summer Wind',
-<<<<<<< HEAD
-        src: '../assets/sounds/River Flows In You.mp3',
-=======
         src: '../assets/sounds/Summer Wind.mp3',
->>>>>>> main
         duration: '01:50'
     }
 ]
